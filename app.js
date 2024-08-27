aade775ca8f8ca4fdbc60d818ecd7277bdf1185d
Kenneth Terrago <kterrago091893@gmail.com>
2024-08-27 23:56:08 +0800

    Other mouse events

--- app.js
+++ app.js
@@ -4,12 +4,25 @@ const app = Vue.createApp({
             showBooks: true,
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
-            age: 45
+            age: 45,
+            x: 0,
+            y: 0
         }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks
+        },
+        handleEvent(event, data) {
+            if (data) {
+                console.log(event.type, data)
+                return
+            }
+            console.log(event.type)
+        },
+        handleMouseMoveEvent(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 })
