7867f2e3cc4679ea1a00e4329d96c4c592bbdc32
Kenneth Terrago <kterrago091893@gmail.com>
2024-08-27 23:33:24 +0800

    Methods and Click events

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const app = Vue.createApp({
             author: 'Brandon Sanderson',
             age: 45
         }
+    },
+    methods: {
+        changeTitle(title) {
+            this.title = title
+        }
     }
 })
 
