f8c36b76cc299787b2d9472b6dc8d70b04d7ba69
Kenneth Terrago <kterrago091893@gmail.com>
2024-08-29 23:25:52 +0800

    Attribute Binding

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const app = Vue.createApp({
     data() {
         return {
+            url: 'http://www.thenetninja.co.uk',
             showBooks: true,
             books: [
-                {title: 'name of the wind', author: 'patrick rothfus'},
-                {title: 'the way of kings', author: 'brandon sandernon'},
-                {title: 'the last empire', author: 'brandon sanderson'}
+                {title: 'name of the wind', author: 'patrick rothfus', img: 'assets/1.jpg'},
+                {title: 'the way of kings', author: 'brandon sandernon', img: 'assets/2.jpg'},
+                {title: 'the last empire', author: 'brandon sanderson', img: 'assets/3.jpg'}
             ]
         }
     },
