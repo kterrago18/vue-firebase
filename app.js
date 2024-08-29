a641d4b379bb2f406d8c7ecd157c4e6843b53c0b
Kenneth Terrago <kterrago091893@gmail.com>
2024-08-30 00:15:01 +0800

    Dynamic Classes

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = Vue.createApp({
             url: 'http://www.thenetninja.co.uk',
             showBooks: true,
             books: [
-                {title: 'name of the wind', author: 'patrick rothfus', img: 'assets/1.jpg'},
-                {title: 'the way of kings', author: 'brandon sandernon', img: 'assets/2.jpg'},
-                {title: 'the last empire', author: 'brandon sanderson', img: 'assets/3.jpg'}
+                {title: 'name of the wind', author: 'patrick rothfus', img: 'assets/1.jpg', isFav: true},
+                {title: 'the way of kings', author: 'brandon sandernon', img: 'assets/2.jpg', isFav: false},
+                {title: 'the last empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true}
             ]
         }
     },
